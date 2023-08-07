b6847483e1ca3200586f3a62bb203c4ed512e068
Ahyeon <ujubada0206@gmail.com>
2023-08-07 16:43:13 +0900

    [ ADD ] IfaqDAO.java
    faq 아티클 출력 메소드 빠져서 추가

--- FinalProject001/src/com/projectors/mvc/IfaqDAO.java
+++ FinalProject001/src/com/projectors/mvc/IfaqDAO.java
@@ -6,6 +6,7 @@ public interface IfaqDAO
 {
 	public ArrayList<FaqDTO> getFaqList();   // FAQ 리스트 출력
 	
+	public FaqDTO viewFaqDetail();			// 특정 FAQ 아티클 출력 
 	
 	//--[참고] faq 등록/수정/삭제 등은 관리자 기능쪽에.
 }
