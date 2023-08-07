001114477993f9cdf501379e755f6e239c60e370
leejoobbok <wnsqhr1230@gmail.com>
2023-08-07 17:46:10 +0900

    [ADD] ILoginDAO.java
    인터페이스 메소드 수정

--- FinalProject001/src/com/projectors/mvc/ILoginDAO.java
+++ FinalProject001/src/com/projectors/mvc/ILoginDAO.java
@@ -4,9 +4,27 @@ public interface ILoginDAO
 {
 	public int count();
 	
-	public UsersDTO userLogin();
+	//-- 유저 로그인
+	public int userLogin(String userId, String userPw);
+
+	public String getUserPin(String userId, String userPw);
 	
-	public AdminDTO adminLogin();	
+	public int checkWDUser(String pinNo);
+	
+
+	
+	//-- 관리자 로그인
+	public int adminLogin(String adminId, String adminPw);	
+	
+	public String getAdminPin(String adminId, String adminPw);
+	
+	public int checkQuitAdmin(String pinNo);
+	
+
+	
+	//-- 로그인 기록
+	
+	public int loginRec(String pinNo);
 	
 	
 	
