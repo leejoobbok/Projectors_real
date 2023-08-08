25192c6ec437f68c108e0d04d422bf7f50606fb9
JinJuYeon <juuju.00b@gmail.com>
2023-08-08 20:07:41 +0900

    [INITIAL] IRecruitDAO.java
    모집공고 메소드 인터페이스

---
+++ FinalProject001/src/com/projectors/mvc/IRecruitDAO.java
@@ -0,0 +1,15 @@
+package com.projectors.mvc;
+
+import java.util.ArrayList;
+
+public interface IRecruitDAO
+{
+	public ArrayList<RecruitDTO> lists();
+	public ArrayList<String> showTool(String recruitNo);
+	
+	public ArrayList<RecruitDTO> optionRegion();
+	public ArrayList<RecruitDTO> optionDoType();
+	public ArrayList<RecruitDTO> optionPos();
+	
+	public ArrayList<String> subRegionList(int regionNo);
+}
