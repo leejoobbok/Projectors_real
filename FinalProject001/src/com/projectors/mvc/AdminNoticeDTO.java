0023335ea01bda075a6584ffa7351e1778929c36
SIST <sooin.ju@gmail.com>
2023-08-08 10:42:51 +0900

    [UPDATE] AdminNoticeDTO.java
    관리자 번호 session 처리로 가져올 부분 속성값에서 제외

--- FinalProject001/src/com/projectors/mvc/AdminNoticeDTO.java
+++ FinalProject001/src/com/projectors/mvc/AdminNoticeDTO.java
@@ -5,8 +5,6 @@ public class AdminNoticeDTO
 	// 관리자 속성 구성
 	private String adminNoticeNo, pinNo, title, content;
 	//-- 관리자공지번호, 관리자식별번호, 제목, 내용
-	private String adminNo;
-	//-- 어느 관리자가 글을 썼는지 식별할 수 있게 하기 위함
 	
 	// getter / setter
 	public String getAdminNoticeNo()
@@ -47,16 +45,6 @@ public class AdminNoticeDTO
 	public void setContent(String content)
 	{
 		this.content = content;
-	}
-
-	public String getAdminNo()
-	{
-		return adminNo;
-	}
-
-	public void setAdminNo(String adminNo)
-	{
-		this.adminNo = adminNo;
 	}
 
 }
