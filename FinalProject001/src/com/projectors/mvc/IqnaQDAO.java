18b40e04e297efd123ff530bb565dbcd3fb84e4c
Ahyeon <ujubada0206@gmail.com>
2023-08-09 16:17:53 +0900

    [UPDATE] IqnaQDAO.java
    문의 관련 인터페이스에 관리자의 문의 목록 출력 기능 추가

--- FinalProject001/src/com/projectors/mvc/IqnaQDAO.java
+++ FinalProject001/src/com/projectors/mvc/IqnaQDAO.java
@@ -10,6 +10,9 @@ public interface IqnaQDAO // 1:1 문의 질문 관련 DAO 인터페이스
 	// 특정 회원의 본인 질문 목록 출력 (문의번호, 제목, 등록일, 답변여부)
 	public ArrayList<QnaQDTO> getQuestionList(String pinNo); 
 	
+	// [관리자] 의 모든 회원 문의 목록 출력 (문의번호, 작성자, 제목, 등록일, 답변여부)
+	public ArrayList<QnaQDTO> getAllQList(); 
+	
 	// 특정 질문 글 출력 ( 문의번호, 제목, 내용, 등록일) 
 	public QnaQDTO viewQuestionDetail(String questionNo);			
 
