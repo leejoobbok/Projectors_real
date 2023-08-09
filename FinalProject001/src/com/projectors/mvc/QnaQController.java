85a33a26f947156523e071c50e660880af87fc23
Ahyeon <ujubada0206@gmail.com>
2023-08-09 14:25:52 +0900

    [UPDATE] QnaQController.java
    안쓰게 된 구문 삭제

--- FinalProject001/src/com/projectors/mvc/QnaQController.java
+++ FinalProject001/src/com/projectors/mvc/QnaQController.java
@@ -51,23 +51,6 @@ public class QnaQController
 	}
 	
 	
-	// 특정 질문글 출력 (QnAArticle.jsp)
-	//-- 답변이랑 같이 출력하려면 매핑을 묶어야 할 것 같아서 아래처럼 ..
-	/*
-	@RequestMapping(value="/question-article.action", method = RequestMethod.GET)
-	public String questionArticle(Model model)
-	{	
-		String result = "";
-		IqnaQDAO dao = sqlSession.getMapper(IqnaQDAO.class);	
-	
-		model.addAttribute("questionArticle", dao.viewQuestionDetail());
-		
-		//result = "/WEB-INF/view/QnAArticle.jsp"; 
-		result = "QnAArticle.jsp";
-		return result; 
-	}
-	*/
-	
 	// (유저) 특정 질문 아티클 출력 (답변 포함) (QnAArticle.jsp)
 	@RequestMapping(value = "/question-article.action", method = RequestMethod.GET)
 	public String combinedData(String questionNo, Model model) {
