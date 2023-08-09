c74d9cbf0f337ef9246175053ebce144fdd6786d
Ahyeon <ujubada0206@gmail.com>
2023-08-09 11:45:07 +0900

    [UPDATE] IqnaADAO.java
    수정중 (관리자 답변용 dao)

--- FinalProject001/src/com/projectors/mvc/IqnaADAO.java
+++ FinalProject001/src/com/projectors/mvc/IqnaADAO.java
@@ -2,9 +2,25 @@ package com.projectors.mvc;
 
 public interface IqnaADAO // 1:1 문의 답변 DAO 인터페이스
 {	
-	/* QnaADTO 의 속성들
-	 	 questionNo, answerNo, answerPinNo, answerContent, answerCreatedDate;
-	    : 질문번호, 답변번호, (관리자)식별번호, 답변내용, 답변등록일 
-	*/
-	public QnaADTO viewAnswerDetail();	// 특정 답변 출력 ( 내용, 등록일) 
+	
+	public QnaADTO viewAnswerDetail();	// 특정 답변 출력 ( 내용, 등록일) //-- 유저, 관리자 공통
+	
+	//========[관리자 기능] 답변 등록,수정,삭제, 최근답변 번호 찾기 ==========
+	
+	// 등록
+	public int addAnswer(QnaADTO dto);
+	
+	// 수정
+	public int updateAnswer(QnaADTO dto);
+	
+	// 삭제
+	public int removeAnswer(String answerNo);
+	
+	// 가장 최근에 생성한 Answer 넘버 가져오기 ( 새 글 등록 후 바로 해당 아티클 확인 가능하도록) 
+	public String findAnswerNo();
 }
+
+/* QnaADTO 의 속성들
+ questionNo, answerNo, answerPinNo, answerContent, answerCreatedDate;
+: 질문번호, 답변번호, (관리자)식별번호, 답변내용, 답변등록일 
+*/
