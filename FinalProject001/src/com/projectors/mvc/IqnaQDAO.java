33ea403dd2b5ea0fb1bd9d78bb6a5275f4e1b7eb
Ahyeon <ujubada0206@gmail.com>
2023-08-10 14:33:44 +0900

    [UPDATE] IqnaQDAO.java
    관리자의 문의 아티클 확인 - 메소드 추가

--- FinalProject001/src/com/projectors/mvc/IqnaQDAO.java
+++ FinalProject001/src/com/projectors/mvc/IqnaQDAO.java
@@ -11,11 +11,16 @@ public interface IqnaQDAO // 1:1 문의 질문 관련 DAO 인터페이스
 	// 특정 회원의 본인 질문 목록 출력 (문의번호, 제목, 등록일, 답변여부)
 	public ArrayList<QnaQDTO> getQuestionList(String pinNo); 
 	
-	// [관리자] 의 모든 회원 문의 목록 출력 (문의번호, 작성자, 제목, 등록일, 답변여부)
-	public ArrayList<QnaQDTO> getAllQList(); 
-	
 	// 특정 질문 글 출력 ( 문의번호, 제목, 내용, 등록일) 
 	public QnaQDTO viewQuestionDetail(String questionNo);		
+	
+	//==============================================================
+	
+	// [관리자] 의 모든 회원 문의 목록 출력 (문의번호,제목,닉네임 등록일, 답변여부)
+	public ArrayList<QnaQDTO> getAllQList(); 
+	
+	// [관리자 ] 의 특정 질문 글 출력 (문의번호, 제목, 닉네임, 핀번호, 내용, 등록일)
+	public QnaQDTO adminQuestionDetail(String questionNO);
 	
 	// [관리자] 의 특정 질문에 대한 답 출력
 	public QnaADTO answerView(String questionNO);
