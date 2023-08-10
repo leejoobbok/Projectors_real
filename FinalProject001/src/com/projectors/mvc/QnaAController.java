4b9355c0f0ab3f8e924527ab21490bc83e49d10e
Ahyeon <ujubada0206@gmail.com>
2023-08-10 12:46:21 +0900

    [UPDATE] QnaAController.java
    답변 등록, 수정, 삭제에 오토커밋 적용하도록 세션 설정 / try- catch로 예외 처리

--- FinalProject001/src/com/projectors/mvc/QnaAController.java
+++ FinalProject001/src/com/projectors/mvc/QnaAController.java
@@ -7,6 +7,7 @@
 package com.projectors.mvc;
 
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +16,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class QnaAController
 {	
+	/*
+	 * @Autowired private SqlSession sqlSession;
+	 */
+	
+	//-- 모든 변경사항 자동 커밋을 위해 (위 세션 객체를 아래처럼 변경..-> 수정중)
 	@Autowired
-	private SqlSession sqlSession; 
+	private SqlSessionFactory sessionFactory;
 	
 	// [ 관리자 ] 답변 등록 (인서트 수행) (QnAQInsert.jsp) 
 	@RequestMapping(value="/insert-answer.action", method = RequestMethod.GET )
@@ -24,37 +30,60 @@ public class QnaAController
 	{
 		String result = "";
 		
-		IqnaADAO dao = sqlSession.getMapper(IqnaADAO.class);
-		dao.addAnswer(dto);
+		try (SqlSession sqlSession = sessionFactory.openSession(true)) //-- 세션을 try 구문에서 정의 
+		{
+			IqnaADAO dao = sqlSession.getMapper(IqnaADAO.class);
+			dao.addAnswer(dto);
+			
+			result = "redirect:q-article-4admin.action?questionNo=" + dto.getQuestionNo();   //-- 페이지 리로드 
+		} catch (Exception e) {
+			// 예외 처리 로직 추가
+			e.printStackTrace();
+		}
 		
-		result = "redirect:q-article-4admin.action?questionNo=" + dto.getQuestionNo();   //-- 페이지 리로드 
-		return result; 
+		return result;
 	}
 	
 	// [ 관리자 ] 답변 수정 (update)  modifyAnswer
 	@RequestMapping(value="/update-answer.action", method = RequestMethod.GET )
 	public String updateAnswer(QnaADTO dto)
-	{
+	{	
 		String result = "";
 		
-		IqnaADAO dao = sqlSession.getMapper(IqnaADAO.class);
-		dao.updateAnswer(dto);
-		
-		result = "redirect:q-article-4admin.action?questionNo=" + dto.getQuestionNo();   //-- 페이지 리로드  
-		return result; 
+		try (SqlSession sqlSession = sessionFactory.openSession(true)) //-- 세션을 try 구문에서 정의 
+		{
+			IqnaADAO dao = sqlSession.getMapper(IqnaADAO.class);
+			dao.updateAnswer(dto);
+			
+			result = "redirect:q-article-4admin.action?questionNo=" + dto.getQuestionNo();   //-- 페이지 리로드 
+		} catch (Exception e) {
+			// 예외 처리 로직 추가
+			e.printStackTrace();
+		}
+		return result;
 	}
 	
 	// [관리자 ] 답변 삭제 (delete)  deleteAnswer
 	@RequestMapping(value="/answer-delete.action", method = RequestMethod.GET )
-	public String deleteAnswer(String answerNo, QnaADTO dto)
-	{
+	public String deleteAnswer(String questionNo, QnaADTO dto)
+	{	
 		String result = "";
 		
-		IqnaADAO dao = sqlSession.getMapper(IqnaADAO.class);
-		dao.removeAnswer(answerNo);
-		
-		result = "redirect:q-article-4admin.action?questionNo=" + dto.getQuestionNo();   //-- 페이지 리로드 
-		return result; 
+		try (SqlSession sqlSession = sessionFactory.openSession(true)) //-- 세션을 try 구문에서 정의 
+		{
+			IqnaADAO dao = sqlSession.getMapper(IqnaADAO.class);
+			
+			questionNo = dto.getQuestionNo(); //-- 삭제 전 질문 번호 저장 
+			
+			dao.deleteAnswer(questionNo); //-- 제거 
+			
+			result = "redirect:q-article-4admin.action?questionNo=" + questionNo;   //-- 질문 번호로 페이지 리로드 
+		} 
+		catch (Exception e) {
+			// 예외 처리 로직 추가
+			e.printStackTrace();
+		}
+		return result;
 	}
 	
 	
