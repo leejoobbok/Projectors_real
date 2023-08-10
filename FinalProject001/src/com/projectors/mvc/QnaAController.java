07ecc81a1b77ab135209bfeacc04f9c08d748c8f
Ahyeon <ujubada0206@gmail.com>
2023-08-10 23:45:25 +0900

    [UPDATE] QnaAController.java
    주석 수정,추가

--- FinalProject001/src/com/projectors/mvc/QnaAController.java
+++ FinalProject001/src/com/projectors/mvc/QnaAController.java
@@ -20,7 +20,7 @@ public class QnaAController
 	 * @Autowired private SqlSession sqlSession;
 	 */
 	
-	//-- 모든 변경사항 자동 커밋을 위해 (위 세션 객체를 아래처럼 변경..-> 수정중)
+	//-- 모든 변경사항 자동 커밋을 위해 (위 세션 객체를 아래처럼 변경)
 	@Autowired
 	private SqlSessionFactory sessionFactory;
 	
@@ -91,7 +91,7 @@ public class QnaAController
 	
 }
 //-- 아현 메모)
-// 컨트롤러는 질문-답변 같이 출력할 때 충돌 피하기 위해 통합. (08.08 )
+// dto, dao.xml, idao는 질문과 답변 분리했으나, 컨트롤러는 질문-답변 같이 출력할 때 충돌 피하기 위해 통합. (08.08 )
 
 //--> 유저쪽에 답변 확인할 때 같은 주소값으로 요청하느라 충돌나서 이 파일 자체를 주석처리했었음
-//--> 관리자 기능 이어받으면서 이걸 관리자 전용 컨트롤러로 활용 (08.09)
+//--> 관리자의 답변 등록,수정,삭제 컨트롤러로 활용 (08.09)
