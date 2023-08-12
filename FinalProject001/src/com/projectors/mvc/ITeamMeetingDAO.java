d8736a46e3218c556430ad0e77ed91aa6760bac2
Ahyeon <ujubada0206@gmail.com>
2023-08-13 04:44:03 +0900

    [INITIAL] ITeamMeetingDAO.java
    회의록 인터페이스

---
+++ FinalProject001/src/com/projectors/mvc/ITeamMeetingDAO.java
@@ -0,0 +1,16 @@
+package com.projectors.mvc;
+
+import java.util.ArrayList;
+
+public interface ITeamMeetingDAO
+{	
+	public ArrayList<TeamMeetingDTO> getList(); // 회의글 목록(메인)
+	public int add(TeamMeetingDTO dto);			// 등록
+	public int modify(TeamMeetingDTO dto);		// 수정
+	public int delete(String meetingNo);		// 삭제
+	public ArrayList<TeamMeetingDTO> viewArticle(); // 아티클 조회
+	
+}
+// dto 속성
+// 회의게시판번호, 최종합류번호(작성자), 제목, 내용, 첨부파일url, 작성일, 회의날짜
+// meetingNo, finalNo, title, content, fileUrl, createdDate, meetingDate;
