711b869578a39069240ef31b36b9350a78e93b79
Ahyeon <ujubada0206@gmail.com>
2023-08-13 20:37:43 +0900

    [INITIAL] FeedController.java
    담벼락 컨트롤러

---
+++ FinalProject001/src/com/projectors/mvc/FeedController.java
@@ -0,0 +1,46 @@
+package com.projectors.mvc;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FeedController
+{
+	@Autowired
+	private SqlSession sqlSession;
+	
+	//● 담벼락 메세지 불러오기 (특정 팀 스페이스)
+	@RequestMapping(value = "/loadFeed.action", method = RequestMethod.GET)
+	public String loadFeed(Model model)
+	{
+		String result = "";
+		
+		IFeedDAO dao = sqlSession.getMapper(IFeedDAO.class);
+		
+		model.addAttribute("list", dao.loadFeed());
+		
+		System.out.println(dao.loadFeed());
+		result = "Feed.jsp";
+		
+		return result;
+	}
+
+	// ● 담벼락 글 등록 
+	@RequestMapping (value = "/addFeed.action", method = RequestMethod.GET)
+	public String addFeed(FeedDTO dto)
+	{
+		String result = "";
+		
+		IFeedDAO dao = sqlSession.getMapper(IFeedDAO.class);
+		dao.addFeed(dto); 
+		
+		result="redirect:loadFeed.action";
+		
+		return result;
+	}
+	
+}
