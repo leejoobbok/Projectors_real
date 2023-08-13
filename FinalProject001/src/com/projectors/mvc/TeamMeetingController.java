625eee91145390e2dd3afbe08027ccf61fe5781b
Ahyeon <ujubada0206@gmail.com>
2023-08-14 03:48:38 +0900

    [INITIAL] TeamMeetingController.java
    회의록 게시판 컨트롤러

---
+++ FinalProject001/src/com/projectors/mvc/TeamMeetingController.java
@@ -0,0 +1,76 @@
+package com.projectors.mvc;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TeamMeetingController
+{
+	@Autowired
+	private SqlSession sqlSession;
+	
+	// ● 회의록 등록 
+	@RequestMapping (value = "/insertMeeting.action", method = RequestMethod.GET)
+	public String meetingInsert(TeamMeetingDTO dto)
+	{
+		String result = "";
+		ITeamMeetingDAO dao = sqlSession.getMapper(ITeamMeetingDAO.class);
+		dao.add(dto);
+		
+		result="TeamMeetingWrite.jsp";
+		return result;
+	}
+	
+	//● 회의록 수정 (작성자 본인만)
+	@RequestMapping(value = "/modifyMeeting.action", method = RequestMethod.GET)
+	public String meetingModify(TeamMeetingDTO dto)
+	{
+		String result = "";
+		ITeamMeetingDAO dao = sqlSession.getMapper(ITeamMeetingDAO.class);
+		dao.modify(dto);
+		
+		result = "redirect:teamMeetingArticle.action?meetingNo=" + dto.getMeetingNo();
+		return result;
+	}
+	
+	//● 회의록 삭제 (작성자 본인만)
+	@RequestMapping(value = "/deleteMeeting.action", method = RequestMethod.GET)
+	public String meetingDelete(String MeetingNo)
+	{
+		String result = "";
+		ITeamMeetingDAO dao = sqlSession.getMapper(ITeamMeetingDAO.class);
+		dao.delete(MeetingNo);
+		
+		result = "redirect:TeamMeetingList.action";
+		return result;
+	}
+	
+	//=========================================================================
+	//● 회의록 리스트로 불러오기 (회의록 게시판 메인)
+	@RequestMapping(value = "/TeamMeetingList.action", method = RequestMethod.GET)
+	public String meetingList(Model model)
+	{
+		String result = "";
+		ITeamMeetingDAO dao = sqlSession.getMapper(ITeamMeetingDAO.class);
+		model.addAttribute("list", dao.getList());
+		
+		result = "TeamMeetingList.jsp";
+		return result;
+	}
+	
+	//● 회의록 아티클 읽기 (모든 팀원)
+	@RequestMapping(value = "/teamMeetingArticle.action", method = RequestMethod.GET)
+	public String meetingArticle(String meetingNo, Model model)
+	{
+		String result = "";
+		ITeamMeetingDAO dao = sqlSession.getMapper(ITeamMeetingDAO.class);
+		model.addAttribute("meetingArticle", dao.viewArticle(meetingNo));
+		
+		result = "TeamMeetingArticle.jsp";
+		return result;
+	}
+}
