4bef74a697d50b696ae728dcd2b0c5d9e2bb347e
leejoobbok <wnsqhr1230@gmail.com>
2023-08-15 06:24:54 +0900

    [add] MyPostController
    경로 변경

--- FinalProject001/src/com/projectors/mvc/MyPostController.java
+++ FinalProject001/src/com/projectors/mvc/MyPostController.java
@@ -1,6 +1,23 @@
 package com.projectors.mvc;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class MyPostController
 {
-
+	@Autowired
+	private SqlSessionFactory sessionFactory;
+	
+	
+	@RequestMapping(value="/mypost.action",method = RequestMethod.GET )
+	public String myApply()
+	{
+		
+		return "/WEB-INF/view/MyPostLists.jsp";
+	}
 }
