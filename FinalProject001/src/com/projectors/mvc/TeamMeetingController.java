9af4c3b97a23e68477a58468bca2bb6b2188f515
leejoobbok <wnsqhr1230@gmail.com>
2023-08-15 13:06:36 +0900

    ]add] TeamMeetiingController.java
    경로변경

--- FinalProject001/src/com/projectors/mvc/TeamMeetingController.java
+++ FinalProject001/src/com/projectors/mvc/TeamMeetingController.java
@@ -21,7 +21,7 @@ public class TeamMeetingController
 		ITeamMeetingDAO dao = sqlSession.getMapper(ITeamMeetingDAO.class);
 		dao.add(dto);
 		
-		result="TeamMeetingWrite.jsp";
+		result="/WEB-INF/view/TeamMeetingWrite.jsp";
 		return result;
 	}
 	
@@ -58,7 +58,7 @@ public class TeamMeetingController
 		ITeamMeetingDAO dao = sqlSession.getMapper(ITeamMeetingDAO.class);
 		model.addAttribute("list", dao.getList());
 		
-		result = "TeamMeetingList.jsp";
+		result = "/WEB-INF/view/TeamMeetingList.jsp";
 		return result;
 	}
 	
@@ -70,7 +70,7 @@ public class TeamMeetingController
 		ITeamMeetingDAO dao = sqlSession.getMapper(ITeamMeetingDAO.class);
 		model.addAttribute("meetingArticle", dao.viewArticle(meetingNo));
 		
-		result = "TeamMeetingArticle.jsp";
+		result = "/WEB-INF/view/TeamMeetingArticle.jsp";
 		return result;
 	}
 }
