c3529a243cfb0eeefa10012038a073dc6b12323c
LJB <wnsqhr1230@gmail.com>
2023-08-31 04:04:53 +0900

    [INITIAL] MenuBarController.java
    유저 페이지 메뉴 바에서 관리자 권한 제한

---
+++ FinalProject001/src/com/projectors/mvc/MenuBarController.java
@@ -0,0 +1,33 @@
+package com.projectors.mvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MenuBarController
+{
+	@Autowired
+	private SqlSession sqlSession;
+	
+
+	@RequestMapping(value = "/checkadmin.action")
+	@ResponseBody
+	public String checkAdmin(String pinNo)
+	{
+		IMenuBarDAO dao = sqlSession.getMapper(IMenuBarDAO.class);
+		
+		String result = "";
+		
+		result = dao.checkAdmin(pinNo);
+				
+		return result;
+	}
+	
+}
