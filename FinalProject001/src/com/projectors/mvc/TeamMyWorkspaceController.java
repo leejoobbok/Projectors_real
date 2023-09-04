6ecfbe4282b922bd36a702036cbfec9a049f17d5
ujubada <ujubada0206@gmail.com>
2023-09-04 16:58:10 +0900

    [INITILAL] TeamMyWorkspaceController.java
    내 작업실 컨트롤러 최초등록 ( 목록, 아티클, 글 등록, 수정, 삭제 )

---
+++ FinalProject001/src/com/projectors/mvc/TeamMyWorkspaceController.java
@@ -0,0 +1,86 @@
+package com.projectors.mvc;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TeamMyWorkspaceController
+{
+	@Autowired
+	private SqlSession sqlSession;
+	
+	//● 작업글 리스트로 불러오기 (내 작업실 게시판 메인)
+	@RequestMapping(value = "/teamMyWorkspace.action", method = RequestMethod.GET)
+	public String meetingList(Model model)
+	{
+		String result = "";
+		ITeamMyWorkspaceDAO dao = sqlSession.getMapper(ITeamMyWorkspaceDAO.class);
+		model.addAttribute("list", dao.getList());
+		
+		result = "/WEB-INF/view/TeamMyWorkspace.jsp";
+		return result;
+	}
+	
+	//● 회의록 아티클 읽기 (모든 팀원)
+	@RequestMapping(value = "/teamMyWorkArticle.action", method = RequestMethod.GET)
+	public String meetingArticle(String workspaceNo, Model model)
+	{
+		String result = "";
+		ITeamMeetingDAO dao = sqlSession.getMapper(ITeamMeetingDAO.class);
+		model.addAttribute("myWorkArticle", dao.viewArticle(workspaceNo));
+		
+		result = "/WEB-INF/view/TeamMyWorkArticle.jsp";
+		return result;
+	}
+	//=========================================================================
+	// ● 작업글 작성 폼 페이지로 이동 
+	@RequestMapping (value = "/writeMyWork.action", method = RequestMethod.GET)
+	public String writeMeeting()
+	{
+		String result = "";
+		result="/WEB-INF/view/TeamMyWorkspaceWrite.jsp";
+		return result;
+	}
+	
+	// ● 작업글 등록 (인서트 수행)
+	@RequestMapping (value = "/insertMyWork.action", method = RequestMethod.GET)
+	public String myWorkInsert(TeamMyWorkspaceDTO dto)
+	{
+		String result = "";
+		ITeamMyWorkspaceDAO dao = sqlSession.getMapper(ITeamMyWorkspaceDAO.class);
+		dao.add(dto);
+		
+		result = "redirect:teamMyWorkspace.action";
+		return result;
+	}
+	
+	//● 작업글 수정 (작성자 본인만)
+	@RequestMapping(value = "/modifyMyWork.action", method = RequestMethod.GET)
+	public String myWorkModify(TeamMyWorkspaceDTO dto)
+	{
+		String result = "";
+		ITeamMyWorkspaceDAO dao = sqlSession.getMapper(ITeamMyWorkspaceDAO.class);
+
+		dao.modify(dto);
+		
+		result = "redirect:teamMyWorkArticle.action?workspaceNo=" + dto.getWorkspaceNo();
+		return result;
+	}
+	
+	//● 작업글 삭제 (작성자 본인만)
+	@RequestMapping(value = "/deleteMyWork.action", method = RequestMethod.GET)
+	public String myWorkDelete(String workspaceNo)
+	{
+		String result = "";
+		ITeamMyWorkspaceDAO dao = sqlSession.getMapper(ITeamMyWorkspaceDAO.class);
+		dao.delete(workspaceNo);
+		
+		result = "redirect:teamMyWorkspace.action";
+		return result;
+	}
+	
+}
